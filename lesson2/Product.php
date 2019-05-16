66ec41bc404076ec5df99b8535647c087eb98079
thienth32 <thienth32@gmail.com>
2019-05-16 10:52:11 +0700

    update

--- lesson2/Product.php
+++ lesson2/Product.php
@@ -1,6 +1,5 @@
 <?php 
 class Product{
-
 	// dinh nghia ra ham khoi tao
 	function __construct(){
 		// thực thi ngay lập tức khi new Object()
@@ -11,20 +10,42 @@ class Product{
 			"root",
 			"123456");
 	}
-
 	function getAll(){
 		$sql = "select * from " . $this->tableName;
-
 		$stmt = $this->conn->prepare($sql);
 		$stmt->execute();
 		return $stmt->fetchAll();
-
 	}
 
-}
-$casio = new Product();
 
-var_dump($casio->getAll());
+	static function all(){
+		$model = new Product();
+		$sql = "select * from " . $model->tableName;
+		$stmt = $model->conn->prepare($sql);
+		$stmt->execute();
+		return $stmt->fetchAll();
+	}
+
+	static function where($col, $con, $val){
+		$model = new Product();
+		$model->queryBuilder = "select * from ". $model->tableName;
+		$model->queryBuilder .= " where $col $con '$val'";
+		return $model;
+	}
+
+	function get(){
+		$stmt = $this->conn->prepare($this->queryBuilder);
+		$stmt->execute();
+		return $stmt->fetchAll();
+	}
+}
+// $casio = new Product();
+
+// var_dump($casio->getAll());
+
+// var_dump(Product::all());
+$products = Product::where('name', 'like', '%Mr%')->get();
+var_dump($products);
 
 
 
