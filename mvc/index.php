45ba864bc55f4513db48c628b335944ac9fa31ad
thienth32 <thienth32@gmail.com>
2019-05-23 10:31:51 +0700

    update

--- mvc/index.php
+++ mvc/index.php
@@ -1,36 +1,7 @@
 <?php 
-require_once './models/Product.php';
-require_once './models/User.php';
-require_once './models/Category.php';
 
 
-// $products = Product::where('name', 'like', '%x%')
-// 					->andWhere('price', '>', 10000)
-// 					->get();
-// var_dump($users);die;
-$products = Product::all();
-// var_dump($products);die;
 
 
+echo "123";
  ?>
- <table>
- 	<thead>
- 		<tr>
- 			<th>Name</th>
- 			<th>Image</th>
- 			<th>Cate name</th>
- 		</tr>
- 	</thead>
- 	<tbody>
- 		<?php foreach ($products as $key => $item): ?>
- 			
- 		<tr>
- 			<td><?= $item->name?></td>
- 			<td>
- 				<img src="<?= $item->image?>" width="80">
- 			</td>
- 			<td><?= $item->getCateName()?></td>
- 		</tr>
- 		<?php endforeach ?>
- 	</tbody>
- </table>
