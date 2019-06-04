7dac1ffed099b162d1c88deaa736688a25085a45
thienth32 <thienth32@gmail.com>
2019-06-04 10:38:11 +0700

    update

--- mvc/index.php
+++ mvc/index.php
@@ -1,34 +1,16 @@
 <?php 
 
 $url = isset($_GET['url']) == true ? $_GET['url'] : "/";
+
+/**
+ * https://github.com/PHPMailer/PHPMailer
+ * https://github.com/jenssegers/blade
+ * https://github.com/illuminate/database
+ * https://github.com/mrjgreen/phroute
+ */
+
 require_once './bootstrap.php';
-use Controllers\HomeController;
-use Controllers\ProductController;
-switch ($url) {
-	case '/':
-		$ctr = new HomeController();
-		$ctr->index();
-		break;
-	case 'chi-tiet':
-		$ctr = new HomeController();
-		$ctr->productDetail();
-		break;
-	case 'admin/products':
-		$ctr = new ProductController();
-		$ctr->listProduct();
-		break;
-	case 'admin/add-product':
-		$ctr = new ProductController();
-		$ctr->addForm();
-		break;
-	case 'admin/save-add-product':
-		$ctr = new ProductController();
-		$ctr->saveAdd();
-		break;
-	
-	default:
-		echo "404 - not found!";
-		break;
-}
+use Routes\CustomRoute;
+CustomRoute::init($url);
 
  ?>
